d6e476f95699da6da09f3b1a9dc16915e6d0ecfc
ndorrh <ndorrh@gmail.com>
2023-01-12 22:59:27 +0100

    Add test for reverse  method in Solver class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,5 +11,11 @@ describe Rentals do
     it 'Should return the factorial of 10  equal 120' do
       expect(@solve.factorial(10)).to eq 3628800
     end
+    it 'Should return the reserve of the string   dance ' do
+      expect(@solve.reverse('dance')).to eq 'ecnad'
+    end
+    it 'Should return the reserve of the string Maximum ' do
+      expect(@solve.reverse('Maximum')).to eq 'mumixaM'
+    end
   end
 end
